aa44922b5d892106604b66163d40bc396f33f09a
Noam Zaks <noamzaks@outlook.com>
2021-05-30 12:55:51 +0300

    Added the resource bar

--- Minigames/Models/Resource.swift
+++ Minigames/Models/Resource.swift
@@ -1,9 +1,21 @@
-import Foundation
+import SwiftUI
 
-enum Resource: CaseIterable {
-    case brick
-    case lumber
-    case ore
-    case grain
-    case wool
+enum Resource: Int, Identifiable, CaseIterable {
+    var id: Int { rawValue }
+    
+    case lumber = 0
+    case brick = 1
+    case grain = 2
+    case wool = 3
+    case ore = 4
+    
+    static let icons: [String] = ["🪵", "🧱", "🌾", "🐑", "🪨"]
+    
+    static let colors: [Color] = [
+        Color(red: 80, green: 50, blue: 50),
+        Color(red: 230, green: 120, blue: 110),
+        Color(red: 230, green: 200, blue: 110),
+        Color(red: 40, green: 170, blue: 40),
+        Color(red: 120, green: 120, blue: 120),
+    ]
 }
--- Minigames/Views/Main.swift
+++ Minigames/Views/Main.swift
@@ -9,3 +9,40 @@ struct MinigamesApp: App {
     }
 }
 
+struct ResourceBar: View {
+    @Environment(\.colorScheme) var scheme
+    
+    let resources: [Resource: UInt]
+    
+    var body: some View {
+        HStack {
+            ForEach(Resource.allCases) { resource in
+                let icon = Resource.icons[resource.rawValue]
+                let count = resources[resource] ?? 0
+                
+                Button(action: {}) {
+                    Capsule()
+                        .fill()
+                        .overlay(Text("\(icon) \(count)").foregroundColor(scheme == .dark ? .black : .white))
+                }.buttonStyle(PlainButtonStyle())
+            }
+        }
+        .frame(maxHeight: 50)
+        .padding()
+    }
+}
+
+struct ResourceBarPreviews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Spacer()
+            ResourceBar(resources: [.lumber: 2])
+        }
+            
+        VStack {
+            Spacer()
+            ResourceBar(resources: [.lumber: 2])
+        }
+        .preferredColorScheme(.dark)
+    }
+}
